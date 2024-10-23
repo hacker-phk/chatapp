9d2117efdcd2520d427085c1fa572c99e443fde9
pujarihemantkumar2003@gmail.com <pujarihemanthkumar2003@gmail.com>
2024-10-23 08:02:52 +0530

    second part complete -ui design

---
+++ frontend/src/components/SideBar/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {BIlogout} from 'react-icons/bi'
+import { CiLogout } from "react-icons/ci";
+
+function LogoutButton() {
+  return (
+    <div className='mt-auto'>
+        <CiLogout className='w-6 h-6 text-white cursor-pointer'/>
+    </div>
+  )
+}
+
+export default LogoutButton
